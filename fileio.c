c90d27a5af5204d062c1dee99fa9bae6efc56f0c
James Fuller <fullerjamesr@gmail.com>
2017-05-23 18:04:25 -0500

    Fixed function to read 3 column data file. It should be more robust to malformed input. Previously this was just a shim put in place to demonstrate the program publicly.

--- fileio.c
+++ fileio.c
@@ -1,33 +1,90 @@
 #include <assert.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 #include "fileio.h"
 
 /*
-Read from a file until the first newline or EOF or error is reached.
-Behavior similar to fgets, except the destination buffer can be realloc'd.
+* Read from a file until the first newline or EOF or error is reached.
+* Behavior is almost identical to fgets, except the number of characters written into the destination is returned.
+*
+* Parameters:
+*   char* buffer: An array of char of at least length `buffer_len`
+*   size_t buffer_len: The maximum number of chars to write to `buffer`
+*   FILE* source: A pointer to a valid FILE object that can be read from
+*   bool incl_newline: When a newline is encountered, should that be written into `buffer`?
+*
+* Returns:
+*   The number of characters that were written into `buffer`, excluding the terminating null character, which is always
+*   written unless `source` has EOF or ERROR set when the function called. Under normal circumstances this should be
+*   identical to the result of calling strlen(buffer) immediately after this function returns.
+*
+* Errors:
+*   If this returns 0 after being called with `buffer_len` > 1, three possibilities exist
+*   (0) there was a blank line
+*   (1) the EOF flag was set on source
+*   (2) the error flag was set on source
+*   In the case of #1 or #2, `buffer` remains unchanged.
+*
+*   Note that if EOF or ERROR results after one or more characters are successfully read, `buffer` will contain those
+*   successfully read characters (and a terminating null) and their count.
+*/
+size_t readline(char *buffer, size_t buffer_len, FILE *source, bool incl_newline)
+{
+    assert(buffer);
+    assert(source);
+    if(feof(source) || ferror(source) || buffer_len == 0)
+        return 0;
+    
+    size_t chars_written = 0;
+    int next_char;
+    
+    while(!ferror(source) && !feof(source) && chars_written < (buffer_len-1))
+    {
+        next_char = fgetc(source);
+        
+        if(next_char == EOF)
+            break;
+        if(!incl_newline && next_char == '\n')
+            break;
+        
+        buffer[chars_written++] = (char)next_char;
+        
+        if(next_char == '\n')
+            break;
+    }
+    buffer[chars_written] = '\0';
+    
+    return chars_written;
+}
 
-Parameters:
-    char** buffer: The line will be read into `buffer`
-    size_t* size: The current size of `buffer`. This will be grown geometrically if buffer is realloc'd
-    FILE* source: A pointer to a valid FILE object that can be read from
-    bool incl_newline: When a newline is encountered, should that be written into `buffer`?
-
-Returns:
-    The number of characters that were written into *buffer, including the CString null terminator.
-    If this returns 0, something went wrong.
+/*
+* Read from a file until the first newline or EOF or error is reached.
+* Behavior similar to fgets, except the destination buffer can be realloc'd.
+*
+* Parameters:
+*   char** buffer: The line will be read into `buffer`
+*   size_t* size: The current size of `buffer`. This (and *buffer) will be grown geometrically if needed.
+*   FILE* source: A pointer to a valid FILE object that can be read from
+*   bool incl_newline: When a newline is encountered, should that be written into `buffer`?
+*
+* Returns:
+*   The number of characters that were written into `buffer`, excluding the terminating null character, which is always
+*   written unless `source` has EOF or ERROR set when the function called. Under normal circumstances this should be
+*   identical to the result of calling strlen(buffer) immediately after this function returns.
+*
+* Errors:
+*   See `readline` above. In addition, if the buffer needs to be grown but a call to realloc fails, the function returns
+*   whatever was able to be successfully read beforehand, just as if EOF had been encountered.
 */
 //TODO: this function responds to EOF and a blank line identically when `incl_newline` is false. This is not good.
-size_t readline(char** buffer, size_t* size, FILE* source, bool incl_newline)
+size_t readline_resizable(char **buffer, size_t *size, FILE *source, bool incl_newline)
 {
+    // sanity checks
+    assert(source);
+    assert(buffer);
+    assert((*buffer));
+    assert(size);
     size_t dest_size = *size;
     char* str = *buffer;
-    
-    // sanity checks
-    assert(source != NULL);
-    assert(buffer != NULL);
-    assert((*buffer) != NULL);
     if(feof(source) || ferror(source) || dest_size < 1)
         return 0;
     
@@ -60,27 +117,44 @@ size_t readline(char** buffer, size_t* size, FILE* source, bool incl_newline)
             break;
     }
     
-    str[chars_written++] = '\0';
+    str[chars_written] = '\0';
     *buffer = str;
     *size = dest_size;
     return chars_written;
 }
 
-//TODO: This is a not very good stopgap for demonstration purposes.
+/*
+ *
+ * Read a 3 column data file, either whitespace or comma delimited, of floating point numbers Each column is read into
+ * an array. Any line that does not consist of 1 or more floating point values separated by whitespace or commas is
+ * ignored. Any missing values are filled with 0.0
+ *
+ * Parameters:
+ *  double** buffer: a two dimensional array of dimension greater than or equal to [3][nrows]
+ *  size_t nrows: the maximum number of rows to read from `source` and store in `buffer[column]`
+ *  FILE* source: a valid file to read from
+ *
+ *  Returns:
+ *      The number of lines (rows) successfully read, <= nrows
+ *
+ */
 size_t read_3_column_data(double** buffer, size_t nrows, FILE* source)
 {
     size_t rows = 0;
-    //size_t line_buffer_size = sizeof(char) * 256;
-    //char* line = malloc(line_buffer_size);
     double one, two, three;
     while(!ferror(source) && !feof(source) && rows < nrows)
     {
-        if(fscanf(source, "%lf %lf %lf\n", &one, &two, &three) > 0)
+        // clear prior values
+        one = 0.0, two = 0.0, three = 0.0;
+        // grab up to three double values
+        if(fscanf(source, "%lf%*[, \t]%lf%*[, \t]%lf", &one, &two, &three) > 0)
         {
             buffer[0][rows] = one;
             buffer[1][rows] = two;
             buffer[2][rows++] = three;
         }
+        // move the stream ahead until a newline appears
+        while(!ferror(source) && !feof(source) && fgetc(source) != '\n');
     }
     return rows;
 }
--- fileio.h
+++ fileio.h
@@ -5,10 +5,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-size_t readline(char** buffer, size_t* size, FILE* source, bool incl_newline);
-//size_t guess_file_length(FILE* source);
-//size_t read_all_lines_from_file(FILE* source,char*** destination, bool incl_newline);
-//size_t read_all_lines_from_filename(char* filename,char*** destination);
+size_t readline_resizable(char **buffer, size_t *size, FILE *source, bool incl_newline);
+size_t readline(char* buffer, size_t buffer_leng, FILE* source, bool incl_newline);
 size_t read_3_column_data(double** buffer, size_t nrows, FILE* source);
 
 #endif //FASTCAT_FILEIO_H
